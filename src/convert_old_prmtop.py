0b17190ac87f99bb90af7290803d9619c40c79f8
Robin Betz <robin@robinbetz.com>
2015-03-25 21:15:36 -0700

    Add script to convert old dabble issued prmtops to vmd-compatible ones

---
+++ src/convert_old_prmtop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Converts a CHARMM format prmtop to a vmd compatible one
+import sys
+from ParmedTools import parmout
+from chemistry.amber import AmberParm
+
+if len(sys.argv) is not 3 :
+  print("USAGE: convert_old_prmtop.py <old prmtop> <new prmtop>")
+  quit(1)
+
+old_prmtop = sys.argv[1];
+new_prmtop = sys.argv[2];
+
+parm = AmberParm(old_prmtop)
+write = parmout(parm, "%s /dev/null vmd" % new_prmtop)
+write.execute()
