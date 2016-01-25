305c24ec7d08f0a8e15c4d8d8f4d6abb77794641
Robin Betz <robin@robinbetz.com>
2016-01-25 14:40:09 -0800

    Minor rho tail test update

--- test/rho_c_tail/test_rho_tail.py
+++ test/rho_c_tail/test_rho_tail.py
@@ -13,13 +13,11 @@ def test_water_box(tmpdir):
     from Dabble import DabbleBuilder
 
     p = str(tmpdir.mkdir("po4_hmr"))
-#    print p+"/test.mae"
     filename =  dir + "rho_test.mae"
     b = DabbleBuilder(solute_filename=filename, output_filename=p+"/test.mae",
                       membrane_system="TIP3", wat_buffer=5.,
                       overwrite=True, tmp_dir=p)
     b.write()
-    #resout, reserr = capfd.readouterr()
     subprocess.check_call(["diff","-q", dir + "test_rho_correct.mae", p+"/test.mae"])
 
 #==============================================================================
@@ -38,8 +36,8 @@ def test_hmr_param(tmpdir):
                     extra_topos=[], extra_params=[])
     w.write(p+"/test")
     subprocess.check_call(["diff", "-q", "--ignore-matching-lines=VERSION",
-                           dir + "test_rho_correct.prmtop",
-                           p+"/test.prmtop"])
+                           dir + "test_rho_correct.psf",
+                           p+"/test.psf"])
 
 #==============================================================================
 
