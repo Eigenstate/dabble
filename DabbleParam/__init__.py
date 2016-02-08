677673cf2353ac50119e1b4102c5784bb6e360ac
Robin Betz <robin@robinbetz.com>
2016-02-08 11:46:48 -0800

    Bump version: 1.2.3 → 1.2.4

--- DabbleParam/__init__.py
+++ DabbleParam/__init__.py
@@ -2,7 +2,7 @@
 Parameterizes molecules for molecular dynamics simulations
 """
 
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 __author__ = 'Robin Betz'
 
 from DabbleParam.charmm import *
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ package_data = {
         }
 
 setup(name='dabble',
-      version='1.2.3',
+      version='1.2.4',
       description='Membrane protein system builder',
       author='Robin Betz',
       author_email='robin@robinbetz.com',
