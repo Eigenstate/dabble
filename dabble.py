0957189629e3bb1e30c033c411a3afe2eb55b1fc
Robin Betz <robin@robinbetz.com>
2016-05-13 11:23:45 -0700

    Bump version: 2.0.1 → 2.0.2

--- dabble.py
+++ dabble.py
@@ -28,7 +28,7 @@ import os, sys
 import signal
 import tempfile
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 __author__ = 'Robin Betz'
     
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
