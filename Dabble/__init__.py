e8a073967d0610cf634baca957349cc98555a136
Robin Betz <robin@robinbetz.com>
2016-12-15 14:30:12 -0800

    Bump version: 2.2.2 → 2.2.3

--- Dabble/__init__.py
+++ Dabble/__init__.py
@@ -1,8 +1,9 @@
 """ Builds membrane protein systems """
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 __author__ = 'Robin Betz'
 
 from Dabble.builder import *
 from Dabble.fileutils import *
+from Dabble.vmdsilencer import VmdSilencer
 
