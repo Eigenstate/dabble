563912c74c5eedb5fccddddd8b4dfa9a75e90d11
Robin Betz <robin@robinbetz.com>
2017-01-04 14:22:44 -0800

    dabble.py -> dabble Silence VMD output on import

--- Dabble/__init__.py
+++ Dabble/__init__.py
@@ -3,7 +3,9 @@
 __version__ = '2.2.3'
 __author__ = 'Robin Betz'
 
-from Dabble.builder import *
-from Dabble.fileutils import *
 from Dabble.vmdsilencer import VmdSilencer
+
+with VmdSilencer():
+    from Dabble.builder import DabbleBuilder
+    from Dabble.fileutils import *
 
