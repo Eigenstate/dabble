c3ab6a1cc9dd23c180ba444b94bd9b2ae703238c
Robin Betz <robin@robinbetz.com>
2017-03-27 16:05:42 -0700

    Bump version: 2.3.4 → 2.3.5

--- Dabble/param/__init__.py
+++ Dabble/param/__init__.py
@@ -2,7 +2,7 @@
 Parameterizes molecules for molecular dynamics simulations
 """
 
-__version__ = '2.3.4'
+__version__ = '2.3.5'
 __author__ = 'Robin Betz'
 
 from Dabble.param.moleculematcher import MoleculeMatcher
