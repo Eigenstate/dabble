956c5f4348ea1ee85032c6f48ff63b1baf026333
Robin Betz <robin@robinbetz.com>
2017-05-03 13:00:47 -0700

    Fix relative imports

--- Dabble/molutils.py
+++ Dabble/molutils.py
@@ -35,7 +35,7 @@ except ImportError:
     from vmd import molecule, atomsel
     atomsel = atomsel.atomsel
 
-import fileutils
+from Dabble import fileutils
 # pylint: disable=no-member
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
