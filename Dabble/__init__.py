052c970f24c47769c96f766797d76b57771d69c8
Robin Betz <robin@robinbetz.com>
2017-12-13 08:05:54 -0800

    Bump version: 2.7.0 → 2.7.1

--- Dabble/__init__.py
+++ Dabble/__init__.py
@@ -1,6 +1,6 @@
 """ Builds membrane protein systems """
 
-__version__ = '2.7.0'
+__version__ = '2.7.1'
 __author__ = 'Robin Betz'
 
 import sys
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ package_data = {
         }
 
 setup(name='dabble',
-      version='2.7.0',
+      version='2.7.1',
       description='Membrane protein system builder',
       author='Robin Betz',
       author_email='robin@robinbetz.com',
