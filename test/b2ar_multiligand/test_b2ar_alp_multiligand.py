baacdffd56890dcd5fa6ac1faf5a1d5f7fdc8884
Robin Betz <robin@robinbetz.com>
2017-12-12 20:06:25 -0800

    Update multiligand test case and add a chamber test

--- test/b2ar_multiligand/test_b2ar_alp_multiligand.py
+++ test/b2ar_multiligand/test_b2ar_alp_multiligand.py
@@ -33,6 +33,9 @@ def test_multiligand_building(tmpdir):
 
     # Check all the alprenolols are there
     assert(len(set(atomsel("resname ALP").get("resid"))) == 10)
+    assert len(atomsel("resname ALP")) == 420
+
+    molecule.delete(m2)
 
 #==============================================================================
 
@@ -51,7 +54,8 @@ def test_multiligand_parameterizing(tmpdir):
     w.write(os.path.join(p, "test"))
 
     # Load the created file
-    m2 = molecule.load("psf", os.path.join(p, "test.psf"))
+    m2 = molecule.load("psf", os.path.join(p, "test.psf"),
+                       "pdb", os.path.join(p, "test.pdb"))
     molecule.set_top(m2)
 
     # Check the system is intact
@@ -63,6 +67,13 @@ def test_multiligand_parameterizing(tmpdir):
     # Check for the correct number of alprenolols
     assert len(atomsel("resname ALP")) == 420
     assert set(atomsel("resname ALP").get("resid")) == set(range(1, 11))
+
+    # Check coordinates are unique (not zero)
+    # It's 419 because two of them have the same x
+    assert len(atomsel("resname ALP")) == 420
+    assert len(set(atomsel("resname ALP").get("x"))) == 419
+
+    molecule.delete(m2)
 
 #==============================================================================
 
@@ -82,6 +93,11 @@ def test_multiligand_amber(tmpdir):
                     extra_params=[os.path.join(dir, "alp.frcmod")])
     w.write(os.path.join(p, "test"))
 
+    # Load the built system and see if it works
+    m2 = molecule.load("parm7", os.path.join(p, "test.prmtop"),
+                       "rst7", os.path.join(p, "test.inpcrd"))
+    molecule.set_top(m2)
+
     # Check results
     assert len(set(atomsel("protein").get("resid"))) == 282
     assert len(set(atomsel("resname ACE NME").get("resid"))) == 4
@@ -93,6 +109,54 @@ def test_multiligand_amber(tmpdir):
     assert len(set(atomsel("resname ALP").get("resid"))) == 10
     assert "OX" in set(atomsel("resname ALP").get("name"))
     assert "O1" not in set(atomsel("resname ALP").get("name"))
+
+    # Check coordinates are unique (not zero)
+    # It's 419 because two of them have the same x
+    assert len(atomsel("resname ALP")) == 420
+    assert len(set(atomsel("resname ALP").get("x"))) == 419
+
+    molecule.delete(m2)
+
+#==============================================================================
+
+def test_multiligand_chamber(tmpdir):
+    """
+    Checks that multiple ligands work with AMBER.
+    Should rename some atoms in matching
+    """
+    from vmd import atomsel, molecule
+    from Dabble.param import AmberWriter
+
+    # Parameterize the system. One atom name will be changed.
+    p = str(tmpdir.mkdir("multiligand_amber"))
+    molid = molecule.load("mae", os.path.join(dir, "test_multiligand_correct.mae"))
+    w = AmberWriter(tmp_dir=p, molid=molid, forcefield="charmm",
+                    extra_topos=[os.path.join(dir, "alprenolol.rtf")],
+                    extra_params=[os.path.join(dir, "alprenolol.prm")])
+    w.write(os.path.join(p, "test"))
+
+    # Load result
+    m2 = molecule.load("psf", os.path.join(p, "test.psf"),
+                       "pdb", os.path.join(p, "test.pdb"))
+    molecule.set_top(m2)
+
+    # Check results
+    assert len(set(atomsel("protein").get("resid"))) == 282
+    assert len(set(atomsel("resname ACE NMA").get("resid"))) == 4
+    assert len(atomsel("water")) == 32106
+    assert len(atomsel("same fragment as lipid")) == 12194
+
+    # Check for the corrrect number of alprenolols
+    assert len(atomsel("resname ALP")) == 420
+    assert len(set(atomsel("resname ALP").get("resid"))) == 10
+    assert "O1" in set(atomsel("resname ALP").get("name"))
+    assert "OX" not in set(atomsel("resname ALP").get("name"))
+
+    # Check coordinates are unique (not zero)
+    # Two atoms have the same X but that is okay
+    assert len(set(atomsel("resname ALP").get("x"))) == 419
+
+    molecule.delete(m2)
 
 #==============================================================================
 
